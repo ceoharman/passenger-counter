546b1cae3c58822c715fb759b80b390a3ac2000b
Harman <161106030+ceoharman@users.noreply.github.com>
2024-05-12 09:48:38 -0400

    javascript added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// intialize the count as 0 
+// listen for the clicks on the increment button 
+// increment the count variable when the button is clicked
+// change the count-el in the html to reflect the new count
+
+let count = 0
+
 function increment() {
-  console.log("the button was clicked")
+  count = count + 1
+  console.log(count)
 }
